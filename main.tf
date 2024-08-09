fe6806cc5abc19aba172175345cb0abd5497f165
22e88 <22e88@mailbox.org>
2024-08-09 11:46:49 +0200

    add private network infrastructure and firewall

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+resource "hcloud_firewall" "myfirewall" {
+  name = "my-firewall"
+  rule {
+    description = "allow ssh traffic"
+    direction = "in"
+    protocol  = "tcp"
+    port      = "22"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
+resource "hcloud_network" "project_private_network" {
+  name     = " Project Private Network"
+  ip_range = "10.0.0.0/16"
+}
+
+resource "hcloud_network_subnet" "project_private_network_subnet" {
+  type         = "cloud"
+  network_id   = hcloud_network.project_private_network.id
+  network_zone = "eu-central"
+  ip_range     = "10.0.1.0/24"
+}
+
 resource "hcloud_server" "cp" {
   name               = "mgmtcp0"
   delete_protection  = false
@@ -5,8 +31,11 @@ resource "hcloud_server" "cp" {
   location           = var.location
   image              = var.image
   server_type        = var.server_type
-  ssh_keys = [ data.hcloud_ssh_key.beta-lab_pubkey.id ]
-
+  firewall_ids       = [ hcloud_firewall.myfirewall.id ]
+  ssh_keys           = [ data.hcloud_ssh_key.beta-lab_pubkey.id ]
+  network {
+    network_id = hcloud_network.project_private_network.id
+  }
   connection {
     private_key = file("~/.ssh/id_ed25519")
     host        = self.ipv4_address
@@ -16,6 +45,11 @@ resource "hcloud_server" "cp" {
       "apt update",
     ]
   }
+  # make sure network subnet is already created before server creation:
+  # (do not use ID attribute in 'depends_on'!)
+  depends_on = [
+    hcloud_network_subnet.project_private_network_subnet
+  ]
 }
 
 data "hcloud_ssh_key" "beta-lab_pubkey" {
